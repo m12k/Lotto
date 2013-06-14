4eab52efc361ee96519be4b331aac055fb520cee
m12k <m12k_102@yahoo.com>
2013-06-14 15:11:35 +0800

    all

---
+++ LottoApp/ClassGlobal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+ 
+public static class ClassGlobal 
+{
+
+    public enum Game
+    {
+        Six42 = 1,
+        Six45 = 2,
+        Six49 = 3,
+        Six55 = 4
+    }
+
+    public enum Operation
+    { 
+        INSERT,
+        UPDATE,
+        DELETE
+    }
+
+}
+ 
